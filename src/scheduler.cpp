ada7a753531c1d3172d0feb5d189fddfdccadbc2
Amanieu d'Antras <amanieu@gmail.com>
2013-03-24 19:31:42 +0000

    Fix typo

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -38,7 +38,7 @@
 #include "work_steal_queue.h"
 
 // thread_local keyword support
-#if __GNUC__ * 100 __GNUC_MINOR__ >= 408 && !defined(__MINGW32__)
+#if __GNUC__ * 100 + __GNUC_MINOR__ >= 408 && !defined(__MINGW32__)
 # define HAVE_THREAD_LOCAL
 #endif
 
