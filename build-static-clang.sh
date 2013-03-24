b03f4cd55ab13d3d1f47b3b7135821da48d84d6e
Amanieu d'Antras <amanieu@gmail.com>
2013-03-24 13:22:14 +0000

    Added build scripts for clang.

---
+++ build-static-clang.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Working around http://llvm.org/bugs/show_bug.cgi?id=12730
+clang++ -std=c++11 -Wall -Wextra -pedantic -pthread -Iinclude src/scheduler.cpp -fvisibility=hidden -O3 -DLIBASYNC_BUILD -DLIBASYNC_STATIC -c -o scheduler.o -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_{1,2,4,8}
+ar rcs libasync++.a scheduler.o
