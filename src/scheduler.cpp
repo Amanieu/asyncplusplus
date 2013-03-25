104bb6f4595e43c8a3221544b108871e5f45b40a
Amanieu d'Antras <amanieu@gmail.com>
2013-03-25 16:54:22 +0000

    Add __has_feature check for thread_local keyword support.

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -38,7 +38,8 @@
 #include "work_steal_queue.h"
 
 // thread_local keyword support
-#if __GNUC__ * 100 + __GNUC_MINOR__ >= 408 && !defined(__MINGW32__)
+#if __has_feature(cxx_thread_local) || \
+   (__GNUC__ * 100 + __GNUC_MINOR__ >= 408 && !defined(__MINGW32__))
 # define HAVE_THREAD_LOCAL
 #endif
 
